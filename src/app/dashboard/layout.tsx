50103b91bfea5f384e6ed58bd677479861be915c
hamza bouyoussef <hamza8pro@gmail.com>
2024-08-31 14:35:27 +0100

    creat project dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Lama Dev School Management Dashboard",
+  description: "Next.js School Management System",
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <div className={inter.className}>dashb{children}</div>
+    </html>
+  );
+}
