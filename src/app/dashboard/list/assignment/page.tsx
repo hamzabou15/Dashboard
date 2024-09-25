686c9953e63d25417d6015f5d0e9a07e69202016
hamza bouyoussef <hamza8pro@gmail.com>
2024-09-25 18:32:01 +0100

    responsive

--- src/app/dashboard/list/assignment/page.tsx
+++ src/app/dashboard/list/assignment/page.tsx
@@ -4,12 +4,10 @@ import React from 'react'
 
 
 const Page = () => {
-
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0 border-t-[1px] border-gray-100">
             <AssignmentForm />
         </div>
     );
 }
-
 export default Page
