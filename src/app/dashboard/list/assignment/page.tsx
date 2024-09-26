6be7ff805e64b13d7108754929b10b87a5d339d1
hamza bouyoussef <hamza8pro@gmail.com>
2024-09-26 15:55:44 +0100

    responsive2

--- src/app/dashboard/list/assignment/page.tsx
+++ src/app/dashboard/list/assignment/page.tsx
@@ -2,7 +2,6 @@
 import { AssignmentForm } from '@/components/list/assignmentForm';
 import React from 'react'
 
-
 const Page = () => {
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0 border-t-[1px] border-gray-100">
